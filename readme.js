b842528d8af877490c1f37117dbb7531273fba87
ldmanh179 <109886127+ldmanh179@users.noreply.github.com>
2025-07-22 16:58:52 +0700

    Create readme.js

---
+++ readme.js
@@ -0,0 +1,11 @@
+{
+  "status": "0",
+  "data": "0",
+  "isOriginalURL": "1",
+  "orientation": "0",
+  "safeAreaView": "1",
+  "appsFlyerDevKey": "0",
+  "appleAppID": "0",
+  "regions": "0",
+  "jsBridgeList": "h5i,Post,jsBridge,Choice,eventTracker,jsThirdBridge,clickEvent"
+}
